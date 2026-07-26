fb6afda7207ae73a2a494a5c500564897ac57a39
wecstor1 <wecstor1@gmail.com>
2026-07-26 20:36:58 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 import asyncio
 import logging
+import os
+
+from aiohttp import web
 
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
@@ -8,6 +11,7 @@ from aiogram.enums import ParseMode
 from config import BOT_TOKEN
 from database import init_db
 from handlers import router
+
 
 logging.basicConfig(
     level=logging.INFO,
@@ -21,9 +25,44 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+# ===== Веб-сервер для Render =====
+
+async def health_check(request):
+    return web.Response(
+        text="AnonBot is running!"
+    )
+
+
+async def start_web_server():
+    app = web.Application()
+
+    app.router.add_get("/", health_check)
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+
+    port = int(os.getenv("PORT", 10000))
+
+    site = web.TCPSite(
+        runner,
+        "0.0.0.0",
+        port
+    )
+
+    await site.start()
+
+    logger.info(f"Web server started on port {port}")
+
+
+# ===== Telegram бот =====
+
 async def main():
+
     logger.info("Запуск базы данных...")
     init_db()
+
+    # запускаем веб-порт для Render
+    await start_web_server()
 
     bot = Bot(
         token=BOT_TOKEN,
@@ -40,6 +79,7 @@ async def main():
 
     try:
         await dp.start_polling(bot)
+
     finally:
         await bot.session.close()
 
@@ -47,5 +87,6 @@ async def main():
 if __name__ == "__main__":
     try:
         asyncio.run(main())
+
     except (KeyboardInterrupt, SystemExit):
         logger.info("Бот остановлен.")
